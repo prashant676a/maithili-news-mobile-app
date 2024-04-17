3733c05874f622366a20bf2c1d9d4b9edf68c967
Prashant <prashantbhandari.374@gmail.com>
2024-04-17 20:19:53 +0545

    initialization and basic navigation setup

---
+++ maibert_frontend/screens/DiscoverNews.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function DiscoverNews() {
+  return (
+    <View>
+      <Text>DiscoverNews</Text>
+    </View>
+  )
+}
